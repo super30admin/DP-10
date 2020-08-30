d5402af3aaef5eb7695a4fdcee3b4e765dbe5a8c
Shantanu609 <shantanu_shinde_@mail.fresnostate.edu>
2020-08-29 18:58:59 -0700

    updated Burst Balloon problem

--- BurstBalloon.py
+++ BurstBalloon.py
@@ -40,34 +40,34 @@
 #         return self.result
 
 class Solution:
-    def maxCoins(self, nums):
+    def maxCoins(self, nums: List[int]) -> int:
         if len(nums) == 0:
             return 0 
         n = len(nums)
         dp = [[0 for _ in range(n)] for _ in range(n)]
         
-        for ln in range(1, n+1):
-            for i in range(0, n-ln+1):
-                j = i + ln - 1 
+        for level in range(1, n+1):
+            for i in range(0, n-level+1):
+                j = i+level-1
                 
-                for k in range(i, j+1): # for the subarray i to j 
-                    left, right = 1, 1 
-                    if j != n-1:
-                        right = nums[j+1]
+                for k in range(i, j+1):
+                    left, right, before, after = 1, 1, 0, 0
                     
-                    if i != 0:
+                    if i > 0:
                         left = nums[i-1]
                     
-                    before, after = 0 ,0 
+                    if j < n-1:
+                        right = nums[j+1]
+                    
                     if k != i:
                         before = dp[i][k-1]
                     
                     if k != j:
                         after = dp[k+1][j]
                     
-                    dp[i][j] = max(dp[i][j], before + left*nums[k]*right + after)
-
-        return dp[0][n-1]
+                    dp[i][j] = max(dp[i][j], before + (left * nums[k] * right) + after)
+        
+        return dp[0][-1]
                         
 if __name__ == "__main__":
     s = Solution()
