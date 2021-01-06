b2dbce32115c55864993ac3900603539951ca213
Amrutha Prakash Veena <pvamrutha2509@gmail.com>
2021-01-06 10:49:14 -0500

    Create p1.py

---
+++ p1.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def superEggDrop(self, K, N):
+        """
+        :type K: int
+        :type N: int
+        :rtype: int
+        """
+        
+        dp = [[0]*(K+1) for i in range(N+1)]
+        
+        for attempts in range(1,len(dp)):
+            
+            for eggs in range(1,len(dp[0])):
+                
+                dp[attempts][eggs] = 1+ dp[attempts-1][eggs] + dp[attempts-1][eggs-1]
+                
+                if dp[attempts][eggs] >= N:
+                    return attempts 
