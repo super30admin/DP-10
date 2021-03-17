78b5fd5237a9e877d6c6cf385b0bf2971c3c0dde
Anshul Atriek <atriekak@gmail.com>
2021-03-17 00:15:43 -0400

    Updated the first problem

--- Burst Balloons.py
+++ Burst Balloons.py
@@ -1,8 +1,35 @@
 class Solution:
+    #Solution 1
+    def maxCoins(self, nums: List[int]) -> int:
+        #Approach: Backtracking
+        #Time Complexity: O(n^3)
+        #Space Complexity: O(n^2)
+        #where, n is the length of nums
+        
+        n = len(nums)
+        dp = [[0 for j in range(n)] for i in range(n)]
+        
+        for length in range(n):                               # window size
+            for i in range(n - length):                       # window start index
+                j = i + length                                # window end index
+                for k in range(i, j + 1):                     # index of last burst in window
+                    balloon = nums[k]
+                    left = nums[i - 1] if i - 1 >= 0 else 1
+                    right = nums[j + 1] if j + 1 < n else 1
+                
+                    before = dp[i][k - 1] if k != i else 0
+                    after = dp[k + 1][j] if k != j else 0
+                    
+                    dp[i][j] = max(dp[i][j], before + left * balloon * right + after)
+        
+        return dp[0][-1]
+    
+    #Solution 2
+    """
     def maxCoins(self, nums: List[int]) -> int:
         #Approach: Backtracking
         #Time Complexity: O(n!)
-        #Space Complexity: O(n^2)
+        #Space Complexity: O(1)
         #where, n is the length of nums
         
         self.maxCoins = 0
@@ -25,3 +52,4 @@ class Solution:
             
             #backtrack
             nums.insert(i, popped)
+        """
