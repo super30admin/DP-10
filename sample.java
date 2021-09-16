9e621e494e32c39f118aad0c29367e40370c5358
DasaniAditya <dasani.ad@northeastern.edu>
2021-09-16 17:04:54 -0400

    Completed DP-10

--- sample.java
+++ sample.java
@@ -41,3 +41,25 @@ class Solution {
         return dp[0][n-1];
     }
 }
+
+class Solution {
+    public int superEggDrop(int k, int n) {
+        if( k == 0 && n == 0) {
+            return 0;
+        }
+        if(k == 1) {
+            return n;
+        }
+        int[][] dp = new int[n+1][k+1];
+        int attempts = 0;
+        
+        while(dp[attempts][k] < n) {
+            attempts++;
+            for(int j = 1;j <= k; j++) {
+                dp[attempts][j] = 1 + dp[attempts-1][j-1] + dp[attempts-1][j];
+            }
+        }
+        return attempts;
+    }
+    
+}
