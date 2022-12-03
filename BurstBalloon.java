f0b85348a3027b65189c7eb2291274a6247d6c7c
ajoshi <ajoshi@cvent.com>
2022-12-03 16:51:43 -0500

    Problem 1 - Balloon Burst

---
+++ BurstBalloon.java
@@ -0,0 +1,41 @@
+// Time Complexity: O(n^3)
+// Space Complexity: O(n^2)
+class Solution {
+    public int maxCoins(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+
+        for (int len = 1; len <=n ; len++) {
+            // i is the starging index
+            for (int i=0; i <=n - len; i++) {
+                /// find the end index
+                int j = i + len - 1;
+                for (int k=i; k<= j;k ++) {
+                    int left = 0;
+                    if (k != i) {
+                        left = dp[i][k-1];
+                    }
+                    int right = 0;
+                    if (k !=j) {
+                        right = dp[k+1][j];
+                    }
+
+                    int before = 1;
+                    int after = 1;
+                    if (i != 0) {
+                        before = nums[i-1];
+                    }
+                    if (j != n -1) {
+                        after = nums[j+1];
+                    }
+
+                    dp[i][j] = Math.max(dp[i][j], left + before * nums[k] * after+ right);
+
+                }
+            }
+        }
+        return dp[0][n-1];
+    }
+}
