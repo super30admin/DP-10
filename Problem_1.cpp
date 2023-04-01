8520fe3318710347a47406739d480f892dbd2e13
Nayana S <nayasrin@cisco.com>
2023-03-31 18:04:25 -0700

    Completed DP-10

---
+++ Problem_1.cpp
@@ -0,0 +1,17 @@
+887. Super Egg Drop
+
+
+
+TC O(KlogN)
+SC O(K) 
+
+
+
+    int superEggDrop(int K, int N) {
+        vector<int> dp(K + 1, 0);
+        int m;
+        for (m = 0; dp[K] < N; m++)
+            for (int k = K; k > 0; --k)
+                dp[k] += dp[k - 1] + 1;
+        return m;
+    }
