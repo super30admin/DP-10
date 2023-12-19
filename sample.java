4b61d27671d5ca8beb68aabd4fb1534769f1f297
Apeksha Kotian <apekshakotian@Apekshas-MacBook-Pro.local>
2023-12-19 04:43:23 -0600

    Completed DP-10

--- sample.java
+++ sample.java
@@ -1,7 +1,42 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(n ^ 3)
+// Space Complexity : O(n ^ 2)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : I didnt face any problem while coding this
 
+// ## Problem2: Burst Balloon (https://leetcode.com/problems/burst-balloons/)
 
-// Your code here along with comments explaining your approach
+class Solution {
+    public int maxCoins(int[] nums) {
+        if(nums.length == 0 || nums == null) return 0;
+        int n = nums.length;
+        int [][]dp = new int[n][n];
+        
+        for(int le = 1; le <= n ; le++){
+            for(int i=0; i <= n-le; i++){
+                int j=i+le-1;
+                int max = 0;
+                for(int k=i; k<=j; k++){
+                    int before = 0;
+                    if(i!=k){
+                        before = dp[i][k-1];
+                    }
+                    int after = 0;
+                    if(j != k ){
+                        after = dp[k+1][j];
+                    }
+                    int left = 1;
+                    if(i != 0){
+                        left = nums[i-1];
+                    }
+                    int right = 1;
+                    if(j != n-1){
+                        right = nums[j+1];
+                    }
+                    max = Math.max(max, before + left * nums[k]* right + after);
+                }
+                dp[i][j] = max;
+            }
+        }
+        return dp[0][n-1];
+    }
+}
